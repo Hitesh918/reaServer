f94ab597023a1cc4c404c5db0edff8463d305832
Hitesh918 <118730681+Hitesh918@users.noreply.github.com>
2024-04-22 21:18:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -780,6 +780,9 @@ app.post("/updateClassLink", async (req, res) => {
     }
 });
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
-});
+const PORT=process.env.PORT||5000
+app.listen(PORT)
+
+// app.listen(5000, () => {
+//     console.log('Server is running on port 5000');
+// });
